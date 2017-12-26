e14a44ea2a29c5a59e13f9f5edc28f8fad6bfd89
Jesse Chrestler <jessec@xceleration.com>
2017-12-26 13:13:42 -0500

    adding the ability to do promise channing.

--- src/index.js
+++ src/index.js
@@ -8,14 +8,20 @@ const promiseDispatcher = (fn, { request, success, failure }) => {
 
 // Take a method (from our API service), params, and three named action creators
 // Execute the standard (request -> success | failure) action cycle for that api call
-const promiseDispatchCreator = (fn, { request, success, failure }) => (...params) => {
-  return dispatch => {
+const promiseDispatchCreator = (fn, { request, success, failure }) => (...params) => dispatch =>
+  //adding a return promise so that we can do promise channing
+  new Promise((resolve, reject) => {
     request && dispatch(request(...params));
     return fn(...params)
-      .then(response => dispatch(success(response, ...params)))
-      .catch(error => dispatch(failure(error, ...params)));
-  };
-};
+      .then(response => {
+        dispatch(success(response, ...params));
+        resolve(response);
+      })
+      .catch(error => {
+        dispatch(failure(error, ...params));
+        reject(error);
+      });
+  });
 
 const createActionCreator = name => payload => {
   return {
